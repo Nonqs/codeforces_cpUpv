b2757c5157cf7201ec3dce4f0cc2b661d0141dcc
Tomas Trujillo <tomastru114@gmail.com>
2024-11-09 14:20:22 +0100

    problem solved

--- Rating800/MedianOfAnArray.java
+++ Rating800/MedianOfAnArray.java
@@ -18,63 +18,23 @@ public class MedianOfAnArray {
             int n = Integer.parseInt(t.nextToken());
 
             t = new StringTokenizer(in.readLine());
-            int[] a = new int[n];
+            ArrayList<Integer> a = new ArrayList<>();
 
             for (int j = 0; j < n; j++) {
-                a[j] = Integer.parseInt(t.nextToken());
+                a.add(Integer.parseInt(t.nextToken()));
             }
 
-            int c = 0;
-            int par = n % 2 == 0 ? (1) : (0);
+            Collections.sort(a);
 
-            Arrays.sort(a);
-            int m = a[Math.round(n / 2) - par];
-            int m2;
+            int m = (n - 1) / 2;
+            int indexM = a.lastIndexOf(a.get(m));
+            int count = (indexM - m) + 1;
 
-            do {
+            out.println(count);
 
-                int index = BetterIndex(a, m);
-                a[index]++;
-                Arrays.sort(a);
-                m2 = a[(n / 2) - par];
-                c++;
-            } while (m2 <= m);
-
-            out.println(c);
         }
 
         out.close();
-
     }
 
-    public static int BetterIndex(int[] a, int m) {
-
-        int index = 0;
-        int[] a2 = a.clone();
-
-        for (int i = 0; i < a.length; i++) {
-            a2[i]++;
-
-            Arrays.sort(a2);
-
-            int m2;
-
-            if (a2.length % 2 == 0) {
-                m2 = a2[(a2.length / 2) - 1];
-                index = (a2.length / 2) - 1;
-            } else {
-                m2 = a2[(a2.length / 2)];
-                index = (a2.length / 2);
-            }
-
-            if (m2 > m) {
-                index = i;
-                return index;
-            }
-
-            a2 = a.clone();
-        }
-        return index;
-
-    }
 }
